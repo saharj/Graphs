3f78198085e5165df39e371af91497f5c8b642a8
Sahar Jafari <shr.jafari@gmail.com>
2021-03-12 12:34:17 -0800

    use recursive solution

--- projects/adventure/adv.py
+++ projects/adventure/adv.py
@@ -11,8 +11,8 @@ world = World()
 
 # You may uncomment the smaller graphs for development and testing purposes.
 # map_file = "maps/test_line.txt"
-# map_file = "maps/test_cross.txt"
-map_file = "/Users/sahar/Documents/lambda-excercise/Graphs/projects/adventure/maps/test_loop.txt"
+map_file = "/Users/sahar/Documents/lambda-excercise/Graphs/projects/adventure/maps/test_cross.txt"
+# map_file = "/Users/sahar/Documents/lambda-excercise/Graphs/projects/adventure/maps/test_loop.txt"
 # map_file = "maps/test_loop_fork.txt"
 # map_file = "/Users/sahar/Documents/lambda-excercise/Graphs/projects/adventure/maps/main_maze.txt"
 
@@ -38,9 +38,20 @@ traversal_path = []
 # print(player.travel(direction))
 # print('****')
 
-traversal_graph = {}
-prev_room_ids = []
-prev_dirs = []
+# create an empty dict to keep track of visited rooms (Maybe set works too?)
+# add the first room id to the dic with the value of True
+# define the recursive fn that receives prev room.id and the prev direction
+# in the room
+# if room id exist in the dict, return
+# else, add the current room id to the dic with the val of True
+# create a map for all vailable directions
+# if prev direction is available, update the value of the opposite direction with the prev room id
+# call the fn for any other directions whitout the value
+# if in the current room there is no direction without value,
+# return an obj with the current room data + returned data, and current room id
+# {data: {2: {"n": 1}, 3:{...}}, roomId: 2}
+# return the value of data
+visited_rooms = set()
 opposites = {
     "n": "s",
     "s": "n",
@@ -48,51 +59,59 @@ opposites = {
     "w": "e"
 }
 
-while True:
-    current = player.current_room
+start_room = player.current_room.id
 
-    if len(prev_room_ids) > 0:
-        if traversal_graph[prev_room_ids[-1]][prev_dirs[-1]] is None:
-            traversal_graph[prev_room_ids[-1]][prev_dirs[-1]] = current.id
+
+def visit(prev_room_id=None, prev_direction=""):
+
+    if prev_direction != "":
+        player.travel(prev_direction)
+
+    room = player.current_room
+    next_room_data = {}
+
+    if room.id in visited_rooms:
+
+        if room.id != start_room:
+            player.travel(prev_direction)
+
+        traversal_path.append(opposites[prev_direction])
+        return {"id": room.id, "data": next_room_data}
+
+    visited_rooms.add(room.id)
+    available_dir = room.get_exits()
+    room_map = {room.id: {}}
+    for direction in available_dir:
+        if prev_room_id is not None and direction == opposites[prev_direction]:
+            room_map[room.id][direction] = prev_room_id
         else:
-            player.travel(opposites[prev_dirs[-1]], False)
-            prev_dirs.pop()
-            prev_room_ids.pop()
-            continue
-    # if current room is not in graph add it and it's directions
-    if current.id not in traversal_graph:
-        available_dir = current.get_exits()
-        obj = {}
-        for direction in available_dir:
-            if len(prev_dirs) > 0 and direction == opposites[prev_dirs[-1]]:
-                obj[direction] = prev_room_ids[-1]
-            else:
-                obj[direction] = None
-        traversal_graph[current.id] = obj
-    # For exists of the current room
-    is_end = True
-    for ex in traversal_graph[current.id]:
-        # if exit is not met, travel to that direction
-        if traversal_graph[current.id][ex] is None:
-            prev_room_ids.append(current.id)
-            prev_dirs.append(ex)
-            player.travel(ex, False)
-            is_end = False
-            break
-    # if node doesn't have next room, travel back
-    if is_end:
-        # we have visited all rooms and are back at the start
-        if len(prev_dirs) == 0:
-            break
-        player.travel(opposites[prev_dirs[-1]], False)
-print("VVVVVV")
-print(traversal_graph)
+            room_map[room.id][direction] = None
+
+    for door in room_map[room.id]:
+        if room_map[room.id][door] is None:
+            traversal_path.append(door)
+            next_room = visit(room.id, door)
+            room_map[room.id][door] = next_room["id"]
+            next_room_data = {**next_room["data"], **next_room_data}
+
+    if prev_direction != "":
+        traversal_path.append(opposites[prev_direction])
+
+    next_room_data = {**room_map, **next_room_data}
+    print(next_room_data)
+    return {"id": room.id, "data": next_room_data}
+
+
+ss = visit()
+# print(ss["data"])
 
 
 # TRAVERSAL TEST
 visited_rooms = set()
 player.current_room = world.starting_room
 visited_rooms.add(player.current_room)
+# print("moves")
+# print(len(traversal_path))
 
 for move in traversal_path:
     player.travel(move)
